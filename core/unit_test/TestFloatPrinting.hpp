b9c3b547a8bd2947ad60527840dd923361f03921
Paul Gannay <paul.gannay@cea.fr>
2026-07-28 14:57:16 +0200

    Remove non-working test and improve test failure display
    Signed-off-by: Paul Gannay <paul.gannay@cea.fr>

--- core/unit_test/TestFloatPrinting.hpp
+++ core/unit_test/TestFloatPrinting.hpp
@@ -15,8 +15,12 @@ struct TestFloatPrinting {
     ASSERT_EQ(errors, 0);
   }
 
+#define CHECK(val, ref) to_chars_helper_f(val, ref, __FILE__, __LINE__)
+
   KOKKOS_FUNCTION constexpr int to_chars_helper_f(FloatType val,
-                                                  char const* ref) const {
+                                                  char const* ref,
+                                                  char const* file,
+                                                  int line) const {
     using Kokkos::Impl::strcmp;
     using Kokkos::Impl::strlen;
     using Kokkos::Impl::to_chars_f;
@@ -27,7 +31,8 @@ struct TestFloatPrinting {
     char* ptr = to_chars_f(buffer, buffer + BUFFER_SIZE, val).ptr;
     *ptr      = '\0';
     if (buffer + strlen(ref) != ptr) {
-      Kokkos::printf("Error: %lx != %lx\n", buffer + strlen(ref), ptr);
+      Kokkos::printf("Error %s:%i:\n", file, line);
+      Kokkos::printf("  %lx != %lx\n", buffer + strlen(ref), ptr);
       if constexpr (std::is_same_v<FloatType, float>) {
         Kokkos::printf("For float %s 0x%x\n", ref,
                        Kokkos::bit_cast<uint32_t>(val));
@@ -39,7 +44,8 @@ struct TestFloatPrinting {
     }
 
     if (strcmp(buffer, ref) != 0) {
-      Kokkos::printf("Error: %s != %s\n", buffer, ref);
+      Kokkos::printf("Error %s:%i:\n", file, line);
+      Kokkos::printf("  %s != %s\n", buffer, ref);
       if constexpr (std::is_same_v<FloatType, float>) {
         Kokkos::printf("For float %s 0x%x\n", ref,
                        Kokkos::bit_cast<uint32_t>(val));
@@ -54,91 +60,86 @@ struct TestFloatPrinting {
   }
 
   KOKKOS_FUNCTION void operator()(int, int& errors) const {
-    errors += to_chars_helper_f(Kokkos::infinity_v<FloatType>, "inf");
-    errors += to_chars_helper_f(-Kokkos::infinity_v<FloatType>, "-inf");
-    errors += to_chars_helper_f(Kokkos::quiet_NaN_v<FloatType>, "nan");
-    errors += to_chars_helper_f(-Kokkos::quiet_NaN_v<FloatType>, "-nan");
-    errors += to_chars_helper_f(Kokkos::signaling_NaN_v<FloatType>, "nan");
-    errors += to_chars_helper_f(-Kokkos::signaling_NaN_v<FloatType>, "-nan");
-    errors += to_chars_helper_f(FloatType(0.), "0.000000e+00");
-    errors += to_chars_helper_f(FloatType(-0.), "-0.000000e+00");
-    errors += to_chars_helper_f(FloatType(1.), "1.000000e+00");
-    errors += to_chars_helper_f(FloatType(-1.), "-1.000000e+00");
-    errors += to_chars_helper_f(FloatType(1.2), "1.200000e+00");
-    errors += to_chars_helper_f(FloatType(-1.2), "-1.200000e+00");
-    errors += to_chars_helper_f(FloatType(5.), "5.000000e+00");
-    errors += to_chars_helper_f(FloatType(50.), "5.000000e+01");
-    errors += to_chars_helper_f(FloatType(0.5), "5.000000e-01");
-    errors += to_chars_helper_f(FloatType(6e20), "6.000000e+20");
-    errors += to_chars_helper_f(FloatType(6e37), "6.000000e+37");
+    errors += CHECK(Kokkos::infinity_v<FloatType>, "inf");
+    errors += CHECK(-Kokkos::infinity_v<FloatType>, "-inf");
+    errors += CHECK(Kokkos::quiet_NaN_v<FloatType>, "nan");
+    errors += CHECK(-Kokkos::quiet_NaN_v<FloatType>, "-nan");
+#if !(defined(KOKKOS_COMPILER_NVHPC) && defined(KOKKOS_ENABLE_OPENACC))
+    // With nvhpc + OpenACC, signaling NaN becomes +inf when copied into a
+    // variable
+    errors += CHECK(Kokkos::signaling_NaN_v<FloatType>, "nan");
+#endif
+    errors += CHECK(-Kokkos::signaling_NaN_v<FloatType>, "-nan");
+    errors += CHECK(FloatType(0.), "0.000000e+00");
+    errors += CHECK(FloatType(-0.), "-0.000000e+00");
+    errors += CHECK(FloatType(1.), "1.000000e+00");
+    errors += CHECK(FloatType(-1.), "-1.000000e+00");
+    errors += CHECK(FloatType(1.2), "1.200000e+00");
+    errors += CHECK(FloatType(-1.2), "-1.200000e+00");
+    errors += CHECK(FloatType(5.), "5.000000e+00");
+    errors += CHECK(FloatType(50.), "5.000000e+01");
+    errors += CHECK(FloatType(0.5), "5.000000e-01");
+    errors += CHECK(FloatType(6e20), "6.000000e+20");
+    errors += CHECK(FloatType(6e37), "6.000000e+37");
 
     // Because of ties-to-even, 10000005 should be rounded down and 10000015
     // should be rounded up.
-    errors += to_chars_helper_f(FloatType(10000005.0), "1.000000e+07");
-    errors += to_chars_helper_f(FloatType(10000015.0), "1.000002e+07");
+    errors += CHECK(FloatType(10000005.0), "1.000000e+07");
+    errors += CHECK(FloatType(10000015.0), "1.000002e+07");
 
     if constexpr (std::is_same_v<FloatType, double>) {
-      errors += to_chars_helper_f(FloatType(1e100), "1.000000e+100");
-      errors +=
-          to_chars_helper_f(Kokkos::denorm_min_v<FloatType>, "4.940656e-324");
-      errors +=
-          to_chars_helper_f(-Kokkos::denorm_min_v<FloatType>, "-4.940656e-324");
-      errors +=
-          to_chars_helper_f(Kokkos::finite_max_v<FloatType>, "1.797693e+308");
-      errors +=
-          to_chars_helper_f(-Kokkos::finite_max_v<FloatType>, "-1.797693e+308");
+      errors += CHECK(FloatType(1e100), "1.000000e+100");
+      errors += CHECK(Kokkos::denorm_min_v<FloatType>, "4.940656e-324");
+      errors += CHECK(-Kokkos::denorm_min_v<FloatType>, "-4.940656e-324");
+      errors += CHECK(Kokkos::finite_max_v<FloatType>, "1.797693e+308");
+      errors += CHECK(-Kokkos::finite_max_v<FloatType>, "-1.797693e+308");
 
       // Numbers just before or after the point were we go from 12 characters
       // to 13.
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x2b617f7d402b1835),
-                                  "1.000000e-99");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x2b617f7d402b1834),
-                                  "1.000000e-99");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x2b617f7d402b1833),
-                                  "9.999999e-100");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x2b617f7d402b1832),
-                                  "9.999999e-100");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x2b617f7d402b1835), "1.000000e-99");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x2b617f7d402b1834), "1.000000e-99");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x2b617f7d402b1833), "9.999999e-100");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x2b617f7d402b1832), "9.999999e-100");
 
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x54b249ad163d7d24),
-                                  "9.999999e+99");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x54b249ad163d7d25),
-                                  "9.999999e+99");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x54b249ad163d7d26),
-                                  "1.000000e+100");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x54b249ad163d7d27),
-                                  "1.000000e+100");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x54b249ad163d7d24), "9.999999e+99");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x54b249ad163d7d25), "9.999999e+99");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x54b249ad163d7d26), "1.000000e+100");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x54b249ad163d7d27), "1.000000e+100");
 
       // Round to nearest, ties to even: 0x51c9bcdf962a0f9f is exactly
       // "9.9999995E85", it should round up)
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x51c9bcdf962a0f9e),
-                                  "9.999999e+85");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x51c9bcdf962a0f9f),
-                                  "1.000000e+86");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x51c9bcdf962a0f9e), "9.999999e+85");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x51c9bcdf962a0f9f), "1.000000e+86");
 
       // Numbers that were incorrectly displayed at some point during the
       // debugging process
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x0000080000000003),
-                                  "4.345847e-311");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x00072800000002AF),
-                                  "9.951990e-309");
-      errors += to_chars_helper_f(Kokkos::bit_cast<double>(0x1cef5f80c024cc14),
-                                  "2.597821e-169");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x0000080000000003), "4.345847e-311");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x00072800000002AF), "9.951990e-309");
+      errors +=
+          CHECK(Kokkos::bit_cast<double>(0x1cef5f80c024cc14), "2.597821e-169");
     }
 
     if constexpr (std::is_same_v<FloatType, float>) {
-      errors +=
-          to_chars_helper_f(Kokkos::denorm_min_v<FloatType>, "1.401298e-45");
-      errors +=
-          to_chars_helper_f(-Kokkos::denorm_min_v<FloatType>, "-1.401298e-45");
-      errors +=
-          to_chars_helper_f(Kokkos::finite_max_v<FloatType>, "3.402823e+38");
-      errors +=
-          to_chars_helper_f(-Kokkos::finite_max_v<FloatType>, "-3.402823e+38");
+      errors += CHECK(Kokkos::denorm_min_v<FloatType>, "1.401298e-45");
+      errors += CHECK(-Kokkos::denorm_min_v<FloatType>, "-1.401298e-45");
+      errors += CHECK(Kokkos::finite_max_v<FloatType>, "3.402823e+38");
+      errors += CHECK(-Kokkos::finite_max_v<FloatType>, "-3.402823e+38");
 
       // Numbers that were incorrectly displayed at some point during the
       // debugging process
-      errors +=
-          to_chars_helper_f(Kokkos::bit_cast<float>(0x9aab), "5.548441e-41");
+      errors += CHECK(Kokkos::bit_cast<float>(0x9aab), "5.548441e-41");
     }
   }
 };
